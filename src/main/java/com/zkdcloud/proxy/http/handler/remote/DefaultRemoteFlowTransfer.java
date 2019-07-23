e6eb04199ec2785432fb971c9974b840a94dcc61
zk-123 <reasuryzk123@gmail.com>
2019-07-23 08:45:48 +0800

    add remove inactive event deal

--- src/main/java/com/zkdcloud/proxy/http/handler/remote/DefaultRemoteFlowTransfer.java
+++ src/main/java/com/zkdcloud/proxy/http/handler/remote/DefaultRemoteFlowTransfer.java
@@ -1,9 +1,12 @@
 package com.zkdcloud.proxy.http.handler.remote;
 
 import com.zkdcloud.proxy.http.context.ChannelContext;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * remote flow exchange
@@ -13,8 +16,21 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  */
 @ChannelHandler.Sharable
 public class DefaultRemoteFlowTransfer extends ChannelInboundHandlerAdapter {
+    /**
+     * static logger
+     */
+    private static Logger logger = LoggerFactory.getLogger(DefaultRemoteFlowTransfer.class);
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ctx.channel().attr(ChannelContext.CLIENT_CHANNEL).get().writeAndFlush(msg);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        Channel clientChannel = ctx.channel().attr(ChannelContext.CLIENT_CHANNEL).get();
+        logger.info("[{}-{}] remote channel inactive.", clientChannel.id().asShortText(), ctx.channel().id().asShortText());
+        ctx.channel().close();
+        clientChannel.close();
     }
 }
